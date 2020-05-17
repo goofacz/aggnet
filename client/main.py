ddc7d523eb01d7c3326ef2889ca41c5bb8fa48a4
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2020-05-18 01:30:36 +0200

    Add userspace client stub
    Signed-off-by: Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>

---
+++ client/main.py
@@ -0,0 +1,27 @@
+import socket
+import select
+
+
+def process_client(address):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(address)
+
+    with open('dev/aggnet0', 'rw') as aggnet:
+        while True:
+            sockets = [aggnet, client]
+            inputs, outputs, exceptions = select.select(sockets, sockets, sockets)
+
+            if aggnet in inputs:
+                pass
+            if aggnet in inputs:
+                pass
+            if client in outputs:
+                pass
+            if client in outputs:
+                pass
+
+def main():
+    process_client(('localhost', 20000))
+
+if __name__ == '__main__':
+    main()
