e71fd06483cd21d4c05db7a945afd38ae99a46e6
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2020-05-21 13:46:06 +0200

    server: Drop broadcast and IPv6 specific frames

--- server/main.py
+++ server/main.py
@@ -1,18 +1,38 @@
 import socket
 import select
+import struct
 
+def aggnet_read(handle):
+    ignored_dest_macs = [b'\xff\xff\xff\xff\xff\xff', b'\x33\x33\x00\x00\x00\x01', b'\x33\x33\x00\x00\x00\x02']
+
+    length = struct.unpack('i', handle.read(4))[0]
+    frame = handle.read(length)
+    if frame[0:6] in ignored_dest_macs:
+        print('drop frame!')
 
 def process_client(client):
-    with open('/dev/aggnet0', 'wb+') as aggnet:
+    with open('/dev/aggnet0', 'r+b') as aggnet:
+        inputs = [aggnet, client]
+        outputs = []
+        exceptions = []
+
         while True:
-            sockets = [client, aggnet]
-            inputs, outputs, exceptions = select.select(sockets, sockets, sockets)
+            inputs, outputs, exceptions = select.select(inputs, outputs, exceptions)
 
             if client in inputs:
+                print('client in')
                 pass
             if client in outputs:
+                print('client out')
                 pass
             if client in exceptions:
+                print('client ex')
+                pass
+            if aggnet in inputs:
+                aggnet_read(aggnet)
+                print('aggnet in')
+            if aggnet in outputs:
+                print('aggnet out')
                 pass
 
 def process_server(port):
